e9ea25f754ed4cea5d54ba6938d5ae87a9ca4296
Ranyelson Neres Carvalho <ranyelson.carvalho@gmail.com>
2019-07-29 10:42:05 -0300

    pagina login - criacao do metodo

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -1,5 +1,17 @@
 <?php
 
+namespace Hcode\Model;
 
+class User {
+
+    //metodo do login
+    public static function login($login, $password){
+        
+        //verificar se ele existe no banco
+        
+
+    }
+
+}
 
 ?>
